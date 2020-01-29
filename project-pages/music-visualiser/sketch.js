6b24801e3953defff4bb79467b0af73ba0c209ee
Joel Smith <joel.smith2226@gmail.com>
2020-01-30 00:14:09 +1100

    Added much more animation. 3 tiered circle for high/mid/low frequencies. Symmetrical circle art and spectograms. Colour mapping fixed (broken for outer layers). Added star particle effect

--- project-pages/music-visualiser/sketch.js
+++ project-pages/music-visualiser/sketch.js
@@ -1,7 +1,7 @@
 // Joel Smith
 // Music Visualizer
 
-var song;
+// GLOBALS
 var fft;
 var button;
 var shapeButton;
@@ -13,6 +13,47 @@ var trackNum;
 var rotation;
 var magnify;
 var magDir;
+var midRing;
+var highRing;
+var bands;
+var stars;
+
+// CLASSES
+class Star{
+  constructor(height, width) {
+    this.x = random(-width/1.5, width/1.5);
+    this.y = random(-height/1.5, height/1.5);
+    this.z = random(width);
+    this.pz = this.z;
+
+  }
+  update() {
+    this.z -= 10;
+    if (this.z < 1) {
+      this.x = random(-width/1.5, width/1.5);
+      this.y = random(-height/1.5, height/1.5);
+      this.z = random(width);
+      this.pz = this.z;
+    }
+  }
+
+  show() {
+     fill(255);
+     noStroke();
+     let sx = map(this.x/this.z, 0, 1, 0, width);
+     let sy = map(this.y/this.z, 0, 1, 0, height);
+     let r = map(this.z, 0, width, 8, 0);
+     //ellipse(sx, sy, r, r);
+     stroke(255);
+     let px = map(this.x/this.pz, 0, 1, 0, width);
+     let py = map(this.y/this.pz, 0, 1, 0, height);
+     line(px,py, sx, sy);
+     this.pz = this.z;
+
+  }
+}
+
+// FUNCTIONS
 
 function toggleSong() {
    if (song.isPlaying()){
@@ -30,7 +71,8 @@ function preload() {
    song1 = loadSound('can-you-feel-it.mp3');
    song2 = loadSound('lucid-dreams.mp3');
    song3 = loadSound('trombones.mp3');
-   songs = [song1, song2, song3]
+   song4 = loadSound('SynthSaga.mp3');
+   songs = [song1, song2, song3, song4]
 }
 
 function changeSong() {
@@ -46,7 +88,6 @@ function changeSong() {
 function setup() {
   var cnv = createCanvas(windowWidth, windowHeight-40);
   cnv.style('display', 'block');
-  colorMode(HSB);
   angleMode(DEGREES);
 
   // Buttons
@@ -64,61 +105,121 @@ function setup() {
   // Global defs
   trackNum = 0;
   circularShape = true;
-  fft = new p5.FFT(0.78, 64);
-  w = width / 64;
+  bands = 128;
+  fft = new p5.FFT(0.78, bands);
+  w = width / (bands * 1.5);
   song = songs[trackNum];
   magDir = 1;
-  rotation = 0;
+  rotation = [0,0,0];
+  midRing = height/5;
+  highRing = height/3;
+  stars = new Array(400);
+  for (var i = 0; i < 400; i++){
+    stars[i] = new Star(windowHeight, windowWidth);
+  }
 }
 
 function draw() {
   background(0);
+
   var spectrum = fft.analyze();
   noStroke(0);
   if (circularShape){
+     translate(width/2, height/2);
      drawCircle(spectrum);
- } else {
+     translate(-width/2, -height/2);
+  } else {
     drawFlat(spectrum);
- }
+  }
+  // stars
+  translate(width/2, height/2);
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
 }
 
 function drawFlat(spectrum){
+  colorMode(HSB, bands);
   var maxF = Math.max(spectrum);
   for (var i = 0; i < spectrum.length; i++) {
      var amp = spectrum[i];
-     var y = map(amp, 0, maxF, height, 0);
+     var y = map(amp, 0, 255, height, 10);
      fill(i,255,255);
-     rect(i*w,y,w-2,height-y)
+     rect(width/2 + i*w,y,w-2,height-y);
+     rect(width/2 - i*w,y,w-2,height-y);
   }
 }
 
 function drawCircle(spectrum){
   noStroke();
-  var maxF = Math.max(spectrum);
-  translate(width/2, height/2);
-
-  // Animation
-  rotate(rotation);
-  rotation += 1;
-  scale(magnify);
-  if (magnify > 5 || magnify < 0.4) {
-     magDir *= -1;
-  }
-  magnify += 0.2 * magDir;
-
-
-
   for (var i = 0; i < spectrum.length; i++) {
-     var amp = spectrum[i];
-     var x = i;
-     var y = map(amp, 0, maxF, height, 0);
-     if (circularShape) {
-        var angle = map(i, 0, spectrum.length, 0, 360);
-        var r = map(amp, 0, 256, 20, 100);
-        x = r * cos(angle);
-        y = r * sin(angle);
+     if (i < spectrum.length/3) {
+        colorMode(HSB, bands/3);
+        lowFreqAnimation(i, spectrum);
+     } else if (i < 2*spectrum.length/3) {
+        colorMode(HSB, 2*bands/3);
+        midFreqAnimation(i, spectrum);
+     } else {
+        colorMode(HSB, bands);
+        highFreqAnimation(i, spectrum);
      }
-     stroke(i, 255, 255);
-     line(0, 0, x, y);
    }
 }
+
+   function lowFreqAnimation(i, spectrum){
+      rotate(rotation[0]);
+      var amp = spectrum[i];
+      var angle = map(i, 0, spectrum.length/3, 0, 180);
+      var r = map(amp, 0, 256, 0, 100);
+      var x = r * cos(angle);
+      var y = r * sin(angle);
+      stroke(i, 255, 255);
+      line(0, 0, x, y);
+      line(0, 0, -x, -y);
+      rotate(-rotation[0]);
+      rotation[0] += 0.1;
+   }
+
+   function midFreqAnimation(i, spectrum){
+      //translate(width/2, height/2);
+      rotate(rotation[1]);
+      var amp = spectrum[i];
+      var angle = map(i, spectrum.length/3, 2*spectrum.length/3, 0, 180);
+      var r = map(amp, 0, 256, 0, 100);
+      var x = r * cos(angle);
+      var y = r * sin(angle);
+      if (x > 0 & y > 0){
+         translateDrawLineReturn(x, y, midRing*x/r, midRing*y/r);
+         translateDrawLineReturn(-x, -y, -midRing*x/r, -midRing*y/r);
+      }
+      rotate(-rotation[1]);
+      rotation[1] -= 0.1;
+   }
+      function highFreqAnimation(i, spectrum){
+         //translate(width/2, height/2);
+         rotate(rotation[2]);
+         var amp = spectrum[i];
+         var angle = map(i, 2*spectrum.length/3, spectrum.length, 0, 360);
+         var r = map(amp, 0, 256, 0, 100);
+         var x = r * cos(angle);
+         var y = r * sin(angle);
+
+         if (x > 0 & y > 0){
+            translateDrawLineReturn(x, y, highRing*x/r, highRing*y/r);
+            translateDrawLineReturn(-x, -y, -highRing*x/r, -highRing*y/r);
+         }
+         rotate(-rotation[2]);
+         rotation[2] += 0.1;
+      }
+
+      function translateDrawLineReturn(x, y, tx, ty){
+         translate(tx, ty);
+         line(0,0, x, y);
+         translate(-tx, -ty);
+      }
+
+
+      function translateDrawLineRetur2n(x, y, rad, angle, amp){
+         arc(x, y, rad, rad, angle, angle + amp, OPEN)
+      }
---
+++ project-pages/music-visualiser/star.js
@@ -0,0 +1,16 @@
+class Star{
+  constructor(height, width) {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.z = random(0, width);
+  }
+  update() {
+
+  }
+
+  show() {
+     fill(255);
+     noStroke();
+     ellipse(this.x, this.y, 8, 8);
+  }
+}
