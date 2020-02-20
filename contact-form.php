0b2b5e035666a7f96c2ca4d8cae206651bb35a43
Joel Smith <joel.smith2226@gmail.com>
2020-02-21 02:10:58 +1100

    come on custom email !!

--- contact-form.php
+++ contact-form.php
@@ -6,11 +6,10 @@ if (isset($_POST['submit-mail'])) {
    $message = $_POST['message'];
 
    $mailTo = "contact@joelsmithdev.com";
-   $headers = "From: ".$mailFrom;
+   $headers = "From:".$mailFrom;
    $subject = "Portfolio inquiry from ".$name.
    $txt = "Portfolio inquiry from ".$name.".\n\n".$message;
 
    mail($mailTo, $subject, $txt, $headers);
-   header("Location: contactme.html?mailsend");
+   header("Location:contactme.html?mailsend");
 }
- ?>
